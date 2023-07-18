b675dda05cf60cdb40a50c3d925176bcc24873d0
[ Yamscool7457] <[ yamscoolodg@gmail.com ]>
2023-07-18 18:42:56 +0000

    aa

---
+++ index.js
@@ -0,0 +1,33 @@
+alert("hello my name is Yamscool !");
+
+
+let total=o;
+function addition(x,y){
+    return x+y;
+    return total;
+}
+function soustraction(x){
+    total-=x;
+    return total;
+}
+
+
+function division(x) {
+    if (total === 0) { return (total= x);}
+    else {
+     total /= x;
+     return total;
+ }
+ }
+ 
+function multiplication(x) {
+   if (total===0) {return (total=x);}
+   else {
+    total *= x;
+    return total;
+}
+}
+
+function reset() {
+    tota = 0;
+}
